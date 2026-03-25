9b146525e7e3d0dda1000653267d03fe84848b83
Only-Adi <adixai2001@gmail.com>
2026-03-26 05:04:32 +0530

    Java Basics Template

---
+++ src/Java/Basics/src/First_Java_Program_setup.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class First_Java_Program_setup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a = "1";
+		System.out.println(a);
+	
+		int b = 2;
+		System.out.println(b); 
+		
+		
+		
+		WebDriver Driver = new ChromeDriver();
+		Driver.get("http://35.154.4.154/medirect_admin/"); 
+		
+		String Value = Driver.getTitle();
+		System.out.println(Value);
+		
+		
+		
+		
+		
+	}
+
+}
