a37fa55682d06341d916d0bb0b0c26568a2ea2f9
Only-Adi <adixai2001@gmail.com>
2026-04-03 02:57:53 +0300

    Added new Code to the Advanced Locators and Identification

--- src/Advanced_locators_and_identification/src/Advanced_Locators_Session_1.java
+++ src/Advanced_locators_and_identification/src/Advanced_Locators_Session_1.java
@@ -1,11 +1,80 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Advanced_Locators_Session_1 {
 
-	public static void Main(String[] args) {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
 		
 		WebDriver Driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(Driver,Duration.ofSeconds(10));
+		
+		
+		// Page 1
+		Driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		WebElement username = Driver.findElement(By.id("inputUsername")); 
+		username.sendKeys("Adixtest");
+		WebElement password = Driver.findElement(By.cssSelector("input[placeholder='Password']"));
+		password.sendKeys("!test@123?");
+		WebElement SignIn = Driver.findElement(By.cssSelector("button[type='submit']"));
+		SignIn.click();
+		
+		String Waitmsg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("error"))).getText();
+		System.out.println("The Error msg is " + Waitmsg); 
+		
+		
+		WebElement ForgotPassword = Driver.findElement(By.linkText("Forgot your password?"));
+		ForgotPassword.click(); 
+		
+		
+		// Page 2 
+		
+		WebElement Name = Driver.findElement(By.cssSelector("input[placeholder='Name']"));
+		Name.sendKeys("Aditya Vshk");
+		
+		WebElement Password = Driver.findElement(By.cssSelector("input[placeholder='Email']"));
+		Password.sendKeys("!Test@123?"); 
+		
+		WebElement Number = Driver.findElement(By.cssSelector("input[placeholder='Phone Number']"));
+		Number.sendKeys("9920149102"); 
+		
+	    WebElement WaitButton = wait.until(ExpectedConditions.elementToBeClickable(By.className("reset-pwd-btn")));
+	    WaitButton.click(); 
+	    
+	    String TempPassword = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("infoMsg"))).getText();
+		System.out.println("The Error msg is " + TempPassword); 
+		
+		 WebElement WaitButtonLogin = wait.until(ExpectedConditions.elementToBeClickable(By.className("go-to-login-btn")));
+		 WaitButtonLogin.click(); 
+		 
+		
+		 
+		 username.sendKeys("Tony Stark");
+		 password.sendKeys("rahulshettyacademy");
+		 
+		 WebElement rememberMyUsername = Driver.findElement(By.id("chkboxOne"));
+		 rememberMyUsername.click();
+		 
+		 SignIn.click();
+		 
+		 
+		 
+	    
+	    
+	    
+	    
+//		WebElement ResetLogin = Driver.findElement(By.className(".reset-pwd-btn"));
+//		ResetLogin.click();
+		
+		
 		
 
 	}
