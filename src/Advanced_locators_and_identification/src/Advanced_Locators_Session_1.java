fdfb1e75fdd434c6e784f8f3c9bba1fac12e6ea5
Only-Adi <adixai2001@gmail.com>
2026-04-03 01:35:54 +0300

    Added new Files and Folders

---
+++ src/Advanced_locators_and_identification/src/Advanced_Locators_Session_1.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Advanced_Locators_Session_1 {
+
+	public static void Main(String[] args) {
+		
+		WebDriver Driver = new ChromeDriver();
+		
+
+	}
+
+}
